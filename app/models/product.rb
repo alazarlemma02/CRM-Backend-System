c2629574a5d6ffa96379550ca320ceb843d212a6
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-24 15:52:53 +0300

    created model for product entity

---
+++ app/models/product.rb
@@ -0,0 +1,6 @@
+class Product < ApplicationRecord
+  belongs_to :product_type
+  belongs_to :product_category
+  belongs_to :product_quantity_type
+  validates :product_name, :product_price, :product_quantity_count, presence: true
+end
