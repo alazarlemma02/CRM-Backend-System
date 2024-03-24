fef87f19ce4f5a4945035441311212fe670fb8ad
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-24 16:02:53 +0300

    spec_file: created model test file for product type entity

---
+++ spec/models/product_type_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe ProductType, type: :model do
+  attributes = [
+    { product_type_name: %i[presence uniqueness] }
+  ]
+  include_examples('model_shared_spec', :product_type, attributes)
+end
