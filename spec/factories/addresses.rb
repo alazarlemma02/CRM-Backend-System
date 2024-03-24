2883c1c1372a4b18c31f29f79ba5ce3aa3405a59
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-24 15:57:23 +0300

    factory: created factory bot file for address entity

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :address do
+    region { Faker::Address.state }
+    city { Faker::Address.city }
+    state { Faker::Address.state_abbr }
+    postal_code { Faker::Address.postcode }
+    country { Faker::Address.country }
+  end
+end
