bd8feee26eff8c64dddff58a3feb1bf6ee60abe2
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-24 16:03:04 +0300

    spec_file: created model test file for role entity

---
+++ spec/models/role_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Role, type: :model do
+  attributes = [
+    { role_name: %i[presence uniqueness] }
+  ]
+  include_examples('model_shared_spec', :role, attributes)
+end
