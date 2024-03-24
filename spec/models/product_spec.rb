d5210aff954b7cd37917644dd3ee64aaed7b585f
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-24 16:02:29 +0300

    spec_file: created model test file for products entity

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  attributes = [
+    { product_name: :presence },
+    { product_price: :presence },
+    { product_quantity_count: :presence },
+    { product_type: :belong_to },
+    { product_category: :belong_to },
+    { product_quantity_type: :belong_to }
+  ]
+  include_examples('model_shared_spec', :product, attributes)
+end
