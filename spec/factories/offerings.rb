963f8e112ab17ad46463ad4ce4b0dc2c9da771a7
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-24 16:00:15 +0300

    factory: created factory bot file for offerings entity

---
+++ spec/factories/offerings.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :offering do
+    offer_title { Faker::Lorem.sentence }
+    offer_start_date { Faker::Time.backward(days: 14) }
+    offer_end_date { Faker::Time.forward(days: 14) }
+    offer_description { Faker::Lorem.paragraph }
+    product
+    offer_type
+  end
+end
