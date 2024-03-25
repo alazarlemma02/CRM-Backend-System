6bd299e4e44039a51921526461dd41de58b40554
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-25 08:51:34 +0300

    new(model): created new user model to combine customer and salesman in to one

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_secure_password
+  belongs_to :address
+  belongs_to :role
+  validates :first_name, :last_name, :email, :phone_number, :password_digest, :user_name, :user_type, :address, :role,
+            presence: true
+  validates :email, :user_name, uniqueness: true
+end
