cb8fec6636dd7d5e588dbd36b2679e678a2f0606
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-25 08:44:19 +0300

    removed: model change to make it in one place

--- spec/models/customer_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Customer, type: :model do
-  attributes = [
-    { customer_name: :presence },
-    { customer_email: %i[presence uniqueness] }
-  ]
-  include_examples('model_shared_spec', :customer, attributes)
-end
