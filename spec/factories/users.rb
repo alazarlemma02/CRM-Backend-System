cf087591df42e7afee9d0d3cce230da0501e07eb
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-25 11:17:26 +0300

    feat: changed user type to be fetched from he model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,13 @@
 
 FactoryBot.define do
   factory :user do
-    first_name { Faker::Name.name }
-    last_name { Faker::Name.name }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
     phone_number { Faker::PhoneNumber.cell_phone }
     profile_picture { Faker::Avatar.image }
     password_digest { Faker::Internet.password }
-    user_type { %w[Customer Salesman].sample }
+    user_type { User.user_types[:customer] }
     date_of_birth { Faker::Date.between(from: 80.years.ago, to: 18.years.ago) }
     user_name { Faker::Internet.username(specifier: 5..8) }
     address
