a7dae9e6fcecacaddab11704721b62841cf33437
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-25 08:50:09 +0300

    modification(model): modified to the new model update

--- spec/models/communication_spec.rb
+++ spec/models/communication_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Communication, type: :model do
@@ -5,8 +7,7 @@ RSpec.describe Communication, type: :model do
     { communication_type: :presence },
     { communication_datetime: :presence },
     { communication_content: :presence },
-    { customer: :belong_to },
-    { salesman: :belong_to }
+    { user: :belong_to }
   ]
   include_examples('model_shared_spec', :communication, attributes)
 end
