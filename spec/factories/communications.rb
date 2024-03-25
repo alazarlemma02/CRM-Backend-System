2aa34306850be80527c92da87017abd340f467ab
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-25 08:54:30 +0300

    new(model-factory): created new communication factory after model modification

--- spec/factories/communications.rb
+++ spec/factories/communications.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :communication do
     communication_type { Faker::Lorem.word }
     communication_datetime { Faker::Time.backward(days: 14) }
     communication_content { Faker::Lorem.paragraph }
-    customer
-    salesman
+    user
   end
 end
