eeac4dab8c507ed07def399271b65396dd432a4b
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-25 08:50:45 +0300

    new(model): created new user model to combine customer and salesman in to one

---
+++ db/migrate/20240325050649_create_users.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :phone_number, null: false
+      t.string :profile_picture
+      t.string :password_digest, null: false
+      t.references :address, null: false, foreign_key: true
+      t.references :role, null: false, foreign_key: true
+      t.string :user_type, null: false
+      t.date :date_of_birth
+      t.string :user_name, null: false
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+    add_index :users, :user_name, unique: true
+  end
+end
