edf36d6968bed8a41bb383475595b0ad0814e4fc
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-25 08:49:27 +0300

    modification(model): modified to the new model update

--- db/migrate/20240324080200_create_communications.rb
+++ db/migrate/20240324080200_create_communications.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 class CreateCommunications < ActiveRecord::Migration[7.1]
   def change
     create_table :communications do |t|
       t.string :communication_type, null: false
       t.datetime :communication_datetime, null: false, default: -> { 'CURRENT_TIMESTAMP' }
       t.text :communication_content, null: false
-      t.references :customer, null: false, foreign_key: true
-      t.references :salesman, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
