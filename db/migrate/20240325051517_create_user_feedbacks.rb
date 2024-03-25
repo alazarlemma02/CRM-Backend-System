0a3cd46f10e0959d3081d223e0d6dc4eab3def18
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-25 08:53:08 +0300

    new(model-migration): created new user_feedback model-migration file after model modification

---
+++ db/migrate/20240325051517_create_user_feedbacks.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateUserFeedbacks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :user_feedbacks do |t|
+      t.string :feedback_title, null: false
+      t.string :feedback_content, null: false
+      t.datetime :feedback_datetime, null: false, default: -> { 'CURRENT_TIMESTAMP' }
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
