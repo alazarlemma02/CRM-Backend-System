63a6bbb8109e38e90201c2728efa00253405ac16
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-26 16:01:12 +0300

    spec(requests): added shared-request and tested it by valid, invalid, and new attributes

---
+++ spec/requests/addresses_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# spec/requests/addresses_spec.rb
+require 'rails_helper'
+RSpec.describe 'Addresses', type: :request do
+  include_examples('request_shared_spec', 'addresses', 6)
+
+  let(:valid_attributes) do
+    {
+      region: Faker::Address.state,
+      city: Faker::Address.city,
+      state: Faker::Address.state_abbr,
+      postal_code: Faker::Address.zip_code,
+      country: Faker::Address.country
+    }
+  end
+
+  let(:invalid_attributes) do
+    {
+      region: Faker::Address.state,
+      city: Faker::Address.city,
+      state: Faker::Address.state_abbr,
+      postal_code: Faker::Address.zip_code,
+      country: nil
+    }
+  end
+
+  let(:new_attributes) do
+    {
+      postal_code: Faker::Address.zip_code
+    }
+  end
+end
