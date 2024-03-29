bd20ee7b9d8c0938c369730adf2c401bdb5c7151
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-29 22:25:07 +0300

    Added 'class: 'Api::V1::Address'' option to existing factory definitions to specify the namespace for model instantiation, ensuring compatibility with the Api::V1 namespace structure.

---
+++ spec/factories/Api/V1/addresses.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :address, class: 'Api::V1::Address' do
+    region { Faker::Address.state }
+    city { Faker::Address.city }
+    state { Faker::Address.state_abbr }
+    postal_code { Faker::Address.postcode }
+    country { Faker::Address.country }
+  end
+end
