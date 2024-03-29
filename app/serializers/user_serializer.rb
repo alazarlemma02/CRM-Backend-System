b66dd83680e950c84d0072691fe0b35e0602fefb
alazarlemma02 <alazar03lemma@gmail.com>
2024-03-29 13:23:17 +0300

    removed password from the json response

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :phone_number, :profile_picture, :password_digest, :address_id,
+  attributes :id, :first_name, :last_name, :email, :phone_number, :profile_picture, :address_id,
              :role_id, :user_type, :date_of_birth, :user_name
 end
