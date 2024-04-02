a84623a687560a5d4b58904f3c43d22fb68e5c1f
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-02 23:19:05 +0300

    renamed Api/V1 directory to lowercase api/v1

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class UsersController < ApplicationController
+      include Common
+      # before_action :find_user, only: %i[show update destroy]
+      skip_before_action :authenticate, only: %i[create]
+
+      private
+
+      def model_params
+        params.require(:payload).permit(:first_name, :last_name, :email, :phone_number, :profile_picture,
+                                        :password, :address_id, :role_id, :user_type, :date_of_birth, :user_name)
+      end
+    end
+  end
+end
