390bd73db75d5bb97648e5ed183f3776cbf4b50d
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-04 22:51:46 +0300

    removed product rating and added average rating

--- spec/factories/api/v1/products.rb
+++ spec/factories/api/v1/products.rb
@@ -4,9 +4,9 @@ FactoryBot.define do
   factory :product, class: 'Api::V1::Product' do
     product_name { Faker::Commerce.product_name }
     product_price { Faker::Commerce.price(range: 10.0..100.0) }
-    product_rating { Faker::Number.between(from: 1, to: 5) }
     product_description { Faker::Lorem.paragraph }
     product_image { Faker::Avatar.image }
+    average_rating { Faker::Number.between(from: 1, to: 5) }
     product_quantity_count { Faker::Number.between(from: 1, to: 100) }
     product_type
     product_category
