430ee50454a5443777b4c88989180e914ee7e762
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-04 22:53:29 +0300

    removed product rating

--- app/serializers/api/v1/product_serializer.rb
+++ app/serializers/api/v1/product_serializer.rb
@@ -3,7 +3,7 @@
 module Api
   module V1
     class ProductSerializer < ActiveModel::Serializer
-      attributes :id, :product_name, :product_price, :product_rating, :product_description, :product_image,
+      attributes :id, :product_name, :product_price, :product_description, :product_image, :average_rating,
                  :product_quantity_count, :product_type_id, :product_category_id, :product_quantity_type_id
     end
   end
