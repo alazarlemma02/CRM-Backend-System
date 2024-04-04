f69c0c47d8173f12f6ebbbde739e985beb27be6f
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-04 22:52:01 +0300

    added product rating

--- spec/factories/api/v1/product_feedbacks.rb
+++ spec/factories/api/v1/product_feedbacks.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :product_feedback, class: 'Api::V1::ProductFeedback' do
     feedback_content { Faker::Lorem.paragraph }
     feedback_datetime { Faker::Time.backward(days: 30) }
+    product_rating { Faker::Number.between(from: 1, to: 5) }
     user
     product
   end
