378089f351705f4c1e0f2b28daf6b63371de80c4
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-04 22:57:16 +0300

    added average rating to the model params

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -9,7 +9,7 @@ module Api
       private
 
       def model_params
-        params.require(:payload).permit(:product_name, :product_price, :product_rating, :product_description,
+        params.require(:payload).permit(:product_name, :product_price, :product_description, :average_rating,
                                         :product_image, :product_quantity_count, :product_type_id, :product_category_id,
                                         :product_quantity_type_id)
       end
