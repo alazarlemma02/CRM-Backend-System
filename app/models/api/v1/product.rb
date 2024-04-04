1239173b8bfeb4d54f69ef518e7cb04ba2c881c0
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-04 22:56:16 +0300

    validated average rating presence

--- app/models/api/v1/product.rb
+++ app/models/api/v1/product.rb
@@ -7,7 +7,7 @@ module Api
       belongs_to :product_category
       belongs_to :product_quantity_type
       has_many :product_feedbacks
-      validates :product_name, :product_price, :product_quantity_count, presence: true
+      validates :product_name, :product_price, :product_quantity_count, :average_rating, presence: true
     end
   end
 end
