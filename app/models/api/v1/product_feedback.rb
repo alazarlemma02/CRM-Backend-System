45acad338600e2ea95f7a6559b9d713d92f4dc01
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-04 22:56:53 +0300

    added a method to update average rating when a product in the database shows a change

--- app/models/api/v1/product_feedback.rb
+++ app/models/api/v1/product_feedback.rb
@@ -5,7 +5,17 @@ module Api
     class ProductFeedback < ApplicationRecord
       belongs_to :user
       belongs_to :product
-      validates :feedback_content, presence: true
+      validates :feedback_content, :product_rating, presence: true
+      validates :user_id,
+                uniqueness: { scope: :product_id, message: 'You have already given feedback for this product' }
+      after_save :update_product_average_rating
+      after_destroy :update_product_average_rating
+
+      private
+
+      def update_product_average_rating
+        product.update(average_rating: product.product_feedbacks.average(:product_rating).to_f.round(2))
+      end
     end
   end
 end
