5134c1ae5cff90703f014e5a4dd49789a609be9b
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-04 20:51:14 +0300

    added product rating

--- db/migrate/20240404110233_create_product_feedbacks.rb
+++ db/migrate/20240404110233_create_product_feedbacks.rb
@@ -3,6 +3,7 @@ class CreateProductFeedbacks < ActiveRecord::Migration[7.1]
     create_table :product_feedbacks do |t|
       t.string :feedback_content, null: false
       t.datetime :feedback_datetime, default: -> { 'CURRENT_TIMESTAMP' }
+      t.float :product_rating
       t.references :user, null: false, foreign_key: true
       t.references :product, null: false, foreign_key: true
 
