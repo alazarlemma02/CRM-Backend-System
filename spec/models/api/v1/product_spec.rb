ee35e4e50cc6c9628a2ef250ea2b7e9f76f40bab
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-06 13:46:34 +0300

    spec: tested active strage image attachement of products

--- spec/models/api/v1/product_spec.rb
+++ spec/models/api/v1/product_spec.rb
@@ -16,6 +16,30 @@ module Api
         { product_feedbacks: :have_many }
       ]
       include_examples('model_shared_spec', :product, attributes)
+
+      describe 'Product active storage' do
+        it { is_expected.to have_many_attached(:product_images) }
+      end
+
+      describe '#product_images_url' do
+        it 'returns the correct URL when product images are attached' do
+          product = create(:product)
+          files = [
+            File.open(Rails.root.join('spec', 'fixtures', 'images', 'product_1.jpeg')),
+            File.open(Rails.root.join('spec', 'fixtures', 'images', 'product_2.jpg'))
+          ]
+          product.product_images.attach(files)
+          expected_urls = product.product_images.map do |product_image|
+            Rails.application.routes.url_helpers.rails_blob_url(product_image, only_path: false)
+          end
+          expect(product.product_images_url.sort).to match_array(expected_urls.sort)
+        end
+
+        it 'returns an empty array when product images are not attached' do
+          product = create(:product)
+          expect(product.product_images_url).to eq []
+        end
+      end
     end
   end
 end
