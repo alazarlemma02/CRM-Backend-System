92979982694246cca5ed9bc944391a0b9f37ec83
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-06 13:47:34 +0300

    tested get request of images of products for index and show actions

--- spec/requests/api/v1/products_spec.rb
+++ spec/requests/api/v1/products_spec.rb
@@ -13,7 +13,6 @@ module Api
           product_name: Faker::Lorem.word,
           product_price: Faker::Number.decimal(l_digits: 2),
           product_description: Faker::Lorem.sentence,
-          product_image: Faker::Avatar.image(size: '50x60'),
           average_rating: Faker::Number.between(from: 1, to: 5),
           product_quantity_count: Faker::Number.between(from: 1, to: 100),
           product_type_id: create(:product_type).id,
@@ -27,7 +26,6 @@ module Api
           product_name: nil,
           product_price: Faker::Number.decimal(l_digits: 2),
           product_description: Faker::Lorem.sentence,
-          product_image: Faker::Avatar.image(size: '50x60'),
           average_rating: Faker::Number.between(from: 1, to: 5),
           product_quantity_count: Faker::Number.between(from: 1, to: 100),
           product_type_id: create(:product_type).id,
@@ -40,6 +38,31 @@ module Api
         {
           product_name: Faker::Lorem.word
         }
+      end
+
+      describe 'GET /api/v1/products' do
+        it 'returns the product images' do
+          count = 2
+          products = create_list(:product, count)
+          expected_urls = []
+          products.each do |product|
+            files = [
+              File.open(Rails.root.join('spec', 'fixtures', 'images', 'product_1.jpeg')),
+              File.open(Rails.root.join('spec', 'fixtures', 'images', 'product_2.jpg'))
+            ]
+            product.product_images.attach(files)
+            expected_urls << product.product_images.map do |product_image|
+              Rails.application.routes.url_helpers.rails_blob_url(product_image, only_path: false)
+            end
+          end
+
+          get(api_v1_products_url, headers: {})
+          result = JSON(response.body)
+
+          expect(response).to have_http_status(:ok)
+          expect(result['data'].count).to eq(count)
+          expect(result['data'].map { |product| product['product_images_url'] }).to eq(expected_urls)
+        end
       end
     end
   end
