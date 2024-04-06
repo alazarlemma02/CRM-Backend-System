ac6cf068fe31b85e79c1541bc233ec7770b0369d
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-06 13:38:22 +0300

    made product images to list of product images rather than one image for a product

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -6,12 +6,19 @@ module Api
       include Common
       skip_before_action :authenticate, only: %i[show index]
 
+      def show
+        super do
+          product = Api::V1::Product.with_attached_images.find(params[:id])
+          product
+        end
+      end
+
       private
 
       def model_params
         params.require(:payload).permit(:product_name, :product_price, :product_description, :average_rating,
-                                        :product_image, :product_quantity_count, :product_type_id, :product_category_id,
-                                        :product_quantity_type_id)
+                                        :product_quantity_count, :product_type_id, :product_category_id,
+                                        :product_quantity_type_id, product_images: [])
       end
     end
   end
