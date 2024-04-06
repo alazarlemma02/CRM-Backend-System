43b4b1de3af6fb9f9a54d23b54cf6da1217c78c4
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-06 13:40:00 +0300

    added product images url to the product model

--- app/models/api/v1/product.rb
+++ app/models/api/v1/product.rb
@@ -7,7 +7,19 @@ module Api
       belongs_to :product_category
       belongs_to :product_quantity_type
       has_many :product_feedbacks
+      has_many_attached :product_images
       validates :product_name, :product_price, :product_quantity_count, :average_rating, presence: true
+
+      def product_images_url
+        if product_images.attached?
+          product_images.map do |image|
+            Rails.application.routes.url_helpers.rails_blob_url(image,
+                                                                only_path: false)
+          end
+        else
+          []
+        end
+      end
     end
   end
 end
