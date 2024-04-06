733b4e2a5b92a926f07e672cc26dd59ff1c55989
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-06 13:43:48 +0300

    migration: migrated the new changes

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +12,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_404_110_233) do
+ActiveRecord::Schema[7.1].define(version: 20_240_405_194_956) do
+  create_table 'active_storage_attachments', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.bigint 'record_id', null: false
+    t.bigint 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
+                                                    unique: true
+  end
+
+  create_table 'active_storage_blobs', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.string 'service_name', null: false
+    t.bigint 'byte_size', null: false
+    t.string 'checksum'
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  end
+
+  create_table 'active_storage_variant_records', force: :cascade do |t|
+    t.bigint 'blob_id', null: false
+    t.string 'variation_digest', null: false
+    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
+  end
+
   create_table 'addresses', force: :cascade do |t|
     t.string 'region', null: false
     t.string 'city', null: false
@@ -89,7 +120,6 @@ ActiveRecord::Schema[7.1].define(version: 20_240_404_110_233) do
     t.decimal 'product_price', null: false
     t.text 'product_description'
     t.float 'average_rating'
-    t.string 'product_image'
     t.integer 'product_quantity_count', null: false
     t.integer 'product_type_id', null: false
     t.integer 'product_category_id', null: false
@@ -123,7 +153,6 @@ ActiveRecord::Schema[7.1].define(version: 20_240_404_110_233) do
     t.string 'last_name', null: false
     t.string 'email', null: false
     t.string 'phone_number', null: false
-    t.string 'profile_picture'
     t.string 'password_digest', null: false
     t.integer 'address_id', null: false
     t.integer 'role_id', null: false
@@ -138,6 +167,8 @@ ActiveRecord::Schema[7.1].define(version: 20_240_404_110_233) do
     t.index ['user_name'], name: 'index_users_on_user_name', unique: true
   end
 
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
   add_foreign_key 'communications', 'users'
   add_foreign_key 'offerings', 'offer_types'
   add_foreign_key 'offerings', 'products'
