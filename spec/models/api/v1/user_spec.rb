ae6fe0d83bb777a65acf6d323709975da5582d83
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-06 13:46:57 +0300

    spec: tested active storage profile picture attachement

--- spec/models/api/v1/user_spec.rb
+++ spec/models/api/v1/user_spec.rb
@@ -23,6 +23,25 @@ module Api
       include_examples('model_shared_spec', :user, attributes)
       it { should define_enum_for(:user_type).with_values(%i[customer salesman]) }
       it { should validate_length_of(:password_digest).is_at_least(6) }
+
+      describe 'User active storage' do
+        it { is_expected.to have_one_attached(:profile_picture) }
+      end
+
+      describe '#profile_picture_url' do
+        it 'returns the correct URL when profile picture is attached' do
+          user = create(:user)
+          file = fixture_file_upload('spec/fixtures/images/user_profile.jpg', 'image/jpg')
+          user.profile_picture.attach(file)
+          expected_url = Rails.application.routes.url_helpers.rails_blob_url(user.profile_picture, only_path: false)
+          expect(user.profile_picture_url).to eq expected_url
+        end
+
+        it 'returns nil when profile picture is not attached' do
+          user = create(:user)
+          expect(user.profile_picture_url).to be_nil
+        end
+      end
     end
   end
 end
