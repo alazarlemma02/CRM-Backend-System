03fc1be97c9c4b95fde39a77583023629d523d5c
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-08 20:48:38 +0300

    remove unecessary configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,6 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
-  config.include Rails.application.routes.url_helpers
 end
 
 Shoulda::Matchers.configure do |config|
