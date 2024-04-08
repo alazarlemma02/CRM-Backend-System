b10a22c1398fac0ce81da3c39b31e8f7814ddfbb
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-08 20:49:54 +0300

    changed from lazy-loading to eager-loading for user creation

--- spec/support/requests/shared_requests.rb
+++ spec/support/requests/shared_requests.rb
@@ -7,7 +7,7 @@ module Api
 
       let(:factory) { controller.classify.underscore.to_sym }
       let(:clazz) { "Api::V1::#{controller.classify}".constantize }
-      let(:user) { create(:user) }
+      let!(:user) { create(:user) }
       let(:token) do
         Api::V1::TokenAuthServices.issue(
           user_id: user.id, email: user.email, phone_number: user.phone_number, user_name: user.user_name
