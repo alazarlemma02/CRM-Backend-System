179ea74f1f9683685f9c54b5639419bca1bb31d9
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-11 23:54:57 +0300

    user: added the relationship between the user and message

--- app/models/api/v1/user.rb
+++ app/models/api/v1/user.rb
@@ -11,6 +11,8 @@ module Api
       has_many :communications
       has_many :product_feedbacks
       has_one_attached :profile_picture
+      has_many :sent_messages, foreign_key: :sender_id, class_name: 'Api::V1::Message'
+      has_many :received_messages, foreign_key: :recipient_id, class_name: 'Api::V1::Message'
       validates :first_name, :last_name, :email, :phone_number, :password_digest, :user_name, :address, :role,
                 presence: true
       validates :email, :user_name, uniqueness: true
