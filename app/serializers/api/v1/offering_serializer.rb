9683210210f93d7d5446c1dbbbb326d58f71242c
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-11 17:53:00 +0300

    serializer: added relationship to serializer

--- app/serializers/api/v1/offering_serializer.rb
+++ app/serializers/api/v1/offering_serializer.rb
@@ -4,6 +4,9 @@ module Api
   module V1
     class OfferingSerializer < ActiveModel::Serializer
       attributes :id, :offer_title, :offer_description, :offer_start_date, :offer_end_date, :product_id, :offer_type_id
+
+      belongs_to :product
+      belongs_to :offer_type
     end
   end
 end
