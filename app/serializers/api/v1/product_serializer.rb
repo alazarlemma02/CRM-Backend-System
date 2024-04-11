87631318d8813669d501e127f0e08bc1f1b28177
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-11 17:53:23 +0300

    serializer: added rationship to the serializer

--- app/serializers/api/v1/product_serializer.rb
+++ app/serializers/api/v1/product_serializer.rb
@@ -6,6 +6,10 @@ module Api
       attributes :id, :product_name, :product_price, :product_description, :average_rating,
                  :product_quantity_count, :product_type_id, :product_category_id, :product_quantity_type_id,
                  :product_images_url
+
+      belongs_to :product_type
+      belongs_to :product_category
+      belongs_to :product_quantity_type
     end
   end
 end
