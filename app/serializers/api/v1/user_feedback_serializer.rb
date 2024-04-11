5cb179f0eee59039a842e9ffa69bbec22bcf5ec7
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-11 17:53:49 +0300

    serializer: added relationship support for the serializer

--- app/serializers/api/v1/user_feedback_serializer.rb
+++ app/serializers/api/v1/user_feedback_serializer.rb
@@ -4,6 +4,8 @@ module Api
   module V1
     class UserFeedbackSerializer < ActiveModel::Serializer
       attributes :id, :feedback_title, :feedback_content, :feedback_datetime, :user_id
+
+      belongs_to :user
     end
   end
 end
