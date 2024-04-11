b252774f56b595e3022e125ce526e7a07026c61e
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-11 23:56:32 +0300

    spec(model): added a test for the message model

---
+++ spec/models/api/v1/message_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Api
+  module V1
+    RSpec.describe Message, type: :model do
+      attributes = [
+        { sender: %i[belong_to presence] },
+        { recipient: %i[belong_to presence] },
+        { content: :presence }
+
+      ]
+      include_examples('model_shared_spec', :message, attributes)
+    end
+  end
+end
