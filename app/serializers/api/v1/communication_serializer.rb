b55afac9b9515abe6196395b5b83a0f11c1067fd
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-12 12:25:46 +0300

    serializer: removed communication serializer

--- app/serializers/api/v1/communication_serializer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Api
-  module V1
-    class CommunicationSerializer < ActiveModel::Serializer
-      attributes :id, :communication_type, :communication_datetime, :communication_content, :user_id
-    end
-  end
-end
