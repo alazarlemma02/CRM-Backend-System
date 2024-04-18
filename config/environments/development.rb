381c023fcab29f01a86ce8bc95251f7a7f7797a0
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-18 18:06:44 +0300

    temporary configuration of email in the development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,21 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
   Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'futurealtech41@gmail.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'futurealtech41@gmail.com',
+    password: 'vsyopypxtjvqhpsb',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    open_timeout: 5,
+    read_timeout: 5
+  }
 end
