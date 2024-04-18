1d2779d4e38d82139181993385d918af90e445ed
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-18 18:05:20 +0300

    feat: added a welcome email method to the new user mailer

---
+++ app/mailers/api/v1/user_mailer.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class UserMailer < ApplicationMailer
+      def welcome_email(user)
+        @user = user
+        mail(to: @user.email, subject: 'Welcome to our System!')
+      end
+    end
+  end
+end
