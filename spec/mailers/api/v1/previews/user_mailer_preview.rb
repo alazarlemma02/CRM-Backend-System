67571be5d7a1b7210d35fff202f75cebe7b7fa99
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-18 18:07:18 +0300

    directory: moved the directory to api/v1/previews

---
+++ spec/mailers/api/v1/previews/user_mailer_preview.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+module Api
+  module V1
+    module Previews
+      class UserMailerPreview < ActionMailer::Preview
+        # code here
+      end
+    end
+  end
+end
