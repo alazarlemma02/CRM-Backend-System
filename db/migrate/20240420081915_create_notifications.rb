93c005a78d5599d09388836d8d37997591f56a1b
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-20 13:27:19 +0300

    migration: added a new table for notification and migrated it

---
+++ db/migrate/20240420081915_create_notifications.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateNotifications < ActiveRecord::Migration[7.1]
+  def change
+    create_table :notifications do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :message, null: false, foreign_key: true
+      t.boolean :status, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
