50c13172f56a9ceb8012d7655da518dcff949e24
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-20 13:25:26 +0300

    feat: added a feature to send notifcation to user after a message is created

--- app/models/api/v1/message.rb
+++ app/models/api/v1/message.rb
@@ -7,6 +7,17 @@ module Api
       belongs_to :recipient, class_name: 'Api::V1::User'
 
       validates :content, :sender, :recipient, presence: true
+      after_create :send_notification_to_recipient, if: :unread?
+
+      private
+
+      def unread?
+        status
+      end
+
+      def send_notification_to_recipient
+        recipient.notifications.create(user: recipient, message: self)
+      end
     end
   end
 end
