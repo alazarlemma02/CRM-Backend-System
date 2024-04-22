974f1759f8b06a629da9909d6a672c20d3f11c2d
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-22 20:17:48 +0300

    fix: added unique to the Faker

--- spec/factories/api/v1/product_types.rb
+++ spec/factories/api/v1/product_types.rb
@@ -2,6 +2,6 @@
 
 FactoryBot.define do
   factory :product_type, class: 'Api::V1::ProductType' do
-    product_type_name { Faker::Commerce.department }
+    product_type_name { Faker::Commerce.unique.department }
   end
 end
