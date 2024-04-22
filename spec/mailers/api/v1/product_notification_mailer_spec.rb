d8e785e3c18dc36c878cc65778701c375c8e602e
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-22 18:16:51 +0300

    spec: added some more tests to test the product mailing functionality

--- spec/mailers/api/v1/product_notification_mailer_spec.rb
+++ spec/mailers/api/v1/product_notification_mailer_spec.rb
@@ -20,6 +20,21 @@ module Api
         it 'does not send new product notification email to salesman' do
           expect(Api::V1::ProductNotificationMailer).not_to receive(:new_product_email).with(salesman, product)
         end
+
+        it 'sends the email to the correct recipients' do
+          Api::V1::ProductNotificationService.notify_customers_about_new_product(product)
+          emails = ActionMailer::Base.deliveries
+          expect(emails.count).to eq(2)
+          expect(emails.map(&:to).flatten).to match_array([customer1.email, customer2.email])
+        end
+
+        it 'sends the email with the correct content' do
+          Api::V1::ProductNotificationService.notify_customers_about_new_product(product)
+          mail = ActionMailer::Base.deliveries.last
+          expect(mail.body.encoded).to include(product.product_name)
+          expect(mail.body.encoded).to include(product.product_description)
+          expect(mail.body.encoded).to include(product.product_price.to_s)
+        end
       end
     end
   end
