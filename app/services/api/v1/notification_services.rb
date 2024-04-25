ee6c9c7e128e42a11db5f8f6d23b6db61920e245
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-25 23:05:32 +0300

    services: removed unecessary frozen comment ruboccop

--- app/services/api/v1/notification_services.rb
+++ app/services/api/v1/notification_services.rb
@@ -1,8 +1,8 @@
-# frozen_string_literal: true
-
 module Api
   module V1
     class NotificationServices
+      # .send_notification_to_recipient
+      # This method send notification when a new message is sent to a recipient
       def self.send_notification_to_recipient(message)
         recipient = message.recipient
         notification = Api::V1::Notification.find_or_initialize_by(user: recipient, message:)
