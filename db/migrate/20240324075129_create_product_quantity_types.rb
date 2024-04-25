3902975c22a545b8377281ccccc5d1de3323ccb5
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-25 23:04:53 +0300

    migration: removed unecessary frozen comment rubocop

--- db/migrate/20240324075129_create_product_quantity_types.rb
+++ db/migrate/20240324075129_create_product_quantity_types.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class CreateProductQuantityTypes < ActiveRecord::Migration[7.1]
   def change
     create_table :product_quantity_types do |t|
--- db/migrate/20240324075955_create_products.rb
+++ db/migrate/20240324075955_create_products.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
-
 class CreateProducts < ActiveRecord::Migration[7.1]
   def change
     create_table :products do |t|
       t.string :product_name, null: false
       t.decimal :product_price, null: false
       t.text :product_description
-      t.float :average_rating
+      t.float :average_rating, default: 0.0
       t.integer :product_quantity_count, null: false
       t.references :product_type, null: false, foreign_key: true
       t.references :product_category, null: false, foreign_key: true
