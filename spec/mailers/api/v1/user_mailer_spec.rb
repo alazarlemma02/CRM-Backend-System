0a16d78ca48f079b06216536006046369ec2de6e
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-27 10:15:53 +0300

    spec: tested the #forgot_password method

--- spec/mailers/api/v1/user_mailer_spec.rb
+++ spec/mailers/api/v1/user_mailer_spec.rb
@@ -12,6 +12,24 @@ module Api
           expect(email.to).to eq([user.email])
         end
       end
+
+      describe '#forgot_password' do
+        let(:user) { create(:user) }
+        let(:mail) { described_class.forgot_password(user) }
+
+        before do
+          user.generate_password_token!
+        end
+
+        it 'renders the headers' do
+          expect(mail.subject).to eq('Password Reset Instructions')
+          expect(mail.to).to eq([user.email])
+        end
+
+        it 'renders the body' do
+          expect(mail.body.encoded).to match(user.reset_password_token)
+        end
+      end
     end
   end
 end
