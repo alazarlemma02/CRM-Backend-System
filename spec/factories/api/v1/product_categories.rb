3a375dc3c06746bbb142bf3ab92ccf9045f527b2
alazarlemma02 <alazar03lemma@gmail.com>
2024-04-27 10:15:29 +0300

    fixed validation error by adding a unique number to it

--- spec/factories/api/v1/product_categories.rb
+++ spec/factories/api/v1/product_categories.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :product_category, class: 'Api::V1::ProductCategory' do
-    product_category_name { Faker::Commerce.department(max: 2) }
+    sequence(:product_category_name) { |n| "#{Faker::Commerce.department(max: 2)}_#{n}" }
   end
 end
