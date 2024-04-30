6fe8cb128e52aa6817b9b911ad9461d76229a00a
alazarlemma02 <alazar03lemma@gmail.com>
2024-05-01 02:12:19 +0300

    modified rendered json message of notification controller

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -5,26 +5,26 @@ module Api
 
       def index
         notifications = @current_user.notifications.all
-        render json: notifications
+        render json: { success: true, data: notifications }, status: :ok
       end
 
       def show
         notification = @current_user.notifications.find(params[:id])
-        render json: notification
+        render json: { success: true, data: notification }, status: :ok
       end
 
       def create
         notification = Api::V1::Notification.new(notification_params)
         if notification.save
-          render json: notification, status: :created
+          render json: { success: true, data: notification }, status: :created
         else
-          render json: notification.errors, status: :unprocessable_entity
+          render json: { success: false, error: notification.errors.full_messages[0]}, status: :unprocessable_entity
         end
       end
 
       def destroy
         @notification.destroy
-        render json: { notification: 'Notification deleted' }
+        render json: { success: true, notification: 'Notification deleted' }, status: :ok
       end
 
       private
