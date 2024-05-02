a20417c780a486d9835409b2db12206e70f145f9
alazarlemma02 <alazar03lemma@gmail.com>
2024-05-02 13:10:06 +0300

    controller: modified to handle admin controll over user registration

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,10 +4,27 @@ module Api
       include Common
       skip_before_action :authenticate, only: %i[create]
       after_action :send_email, only: %i[create]
+      before_action :authenticate_admin, only: %i[create_salesman create_admin]
 
       def show
         super do
           user = Api::V1::User.with_attached_images.find(params[:id])
+          user
+        end
+      end
+
+      def create_salesman
+        create do
+          user = @clazz.new(model_params)
+          user.user_type = :salesman
+          user
+        end
+      end
+
+      def create_admin
+        create do
+          user = @clazz.new(model_params)
+          user.user_type = :admin
           user
         end
       end
@@ -18,9 +35,16 @@ module Api
         Api::V1::UserMailer.welcome_email(@obj).deliver_now if @obj.present?
       end
 
+      def authenticate_admin
+        return if @current_user&.admin?
+
+        render json: { success: false, error: 'Only admin can perform this action' },
+               status: :forbidden
+      end
+
       def model_params
         params.require(:payload).permit(:first_name, :last_name, :email, :phone_number, :profile_picture,
-                                        :password, :address_id, :role_id, :user_type, :date_of_birth, :user_name)
+                                        :password, :address_id, :role_id, :date_of_birth, :user_name)
       end
     end
   end
